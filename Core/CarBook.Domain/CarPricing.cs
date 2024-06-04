b4d703193b5d0b485ac773fa08ac18c2c4adb75b
Melih karakuş <melih.karakus1818@gmail.com>
2024-06-04 20:00:24 +0300

    Add project files.

---
+++ Core/CarBook.Domain/CarPricing.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Domain.Entites
+{
+    public class CarPricing
+    {
+        public int CarPricingID { get; set; }
+        public int CarID { get; set; }
+        public Car Cars { get; set; }
+        public int PricingID { get; set; }
+        public Pricing Pricings { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
