2c94fe3915ab37f941a9028cc082bd7782753e55
Melih karakuş <melih.karakus1818@gmail.com>
2024-07-01 20:07:30 +0300

    1- Fronted kismi eklendi ve UI layout tarafları ayarlandı. 2- RenderBody ayarlandı UI Layout içinde 3- ViewComponent klasörü oluşturuldu. 4- Shared içine Components oluşturuldu ve ViewComponentden çağrılma yapıldı.

---
+++ Frontends/CarBook.WebUI/ViewComponents/UILayout/_MainCoverUILayoutComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarBook.WebUI.ViewComponents.UILayout
+{
+    public class _MainCoverUILayoutComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
